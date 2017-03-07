c2902be3b9ee263c8be2b8506a4d6c91f8e19724
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-03-06 21:41:06 -0700

    login page styled

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-   TextInput, StyleSheet, View
+   TextInput, StyleSheet, View, Button, Text, ActivityIndicator, TouchableHighlight
 } from 'react-native';
 import axios from 'axios';
 import { API_URL } from '../actions';
@@ -16,40 +16,83 @@ class Login extends React.Component {
     this.state = {
       username: '',
       password: '',
+      err: null,
+      loading: false,
     };
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
-
-
+    this.submitLogin = this.submitLogin.bind(this);
   }
 
   handleUsernameChange(username) {
-    console.log(username)
-    this.setState({username})
+    this.setState({ username });
   }
 
   handlePasswordChange(password) {
-    console.log(password)
-    this.setState({password})
+    this.setState({ password });
   }
 
+  submitLogin() {
+    this.setState({ loading: true });
+    fetch(`${API_URL}/login`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: this.state.username,
+      password: this.state.password
+    })
+  })
+  .then(response => response.json())
+  .then((res) => {
+    if (res.err) this.setState({ err: res.err, loading: false })
+    else this.setState({ loading: false })
+  })
+  .catch((err) => {
+    console.log(err);
+    this.setState({ loading: false });
+  });
+}
 
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#565A5C'}}>
+      <View style={{flex: 1, alignItems: 'center', backgroundColor: '#000000'}}>
+
         <TextInput
-          placeholder='Username'
-          style={{flex: 1, color: 'white'}}
+          placeholder='username'
+          placeholderTextColor='white'
+          style={{flex: 1, color: 'white' }}
+          autoCapitalize='none'
+          autoCorrect= {false}
           onChangeText={this.handleUsernameChange}
           value={this.state.username}
         />
 
         <TextInput
-          placeholder='Password'
-          style={{flex: 10, color: 'white'}}
+          placeholder='password'
+          placeholderTextColor='white'
+          style={{flex: 1, color: 'white'}}
+          autoCapitalize='none'
+          autoCorrect={false}
           onChangeText={this.handlePasswordChange}
           value={this.state.password}
         />
+
+        <ActivityIndicator
+          animating={this.state.loading}
+          style={{flex: 1, color: '#CFB87C'}}
+          size='large'
+        />
+
+          {this.state.err ? <Text style={{flex: 1, color: 'white'}}>{this.state.err}</Text> : null}
+
+          <Button
+            style={{backgroundColor: '#CFB87C', color: 'white'}}
+            title="Submit"
+            onPress={this.submitLogin}
+          />
       </View>
     );
   }
